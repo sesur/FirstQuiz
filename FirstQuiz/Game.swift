60f4747ea49024e8aeb6aaa7bc6670d9ba89aef6
Sergiu <sergiusuru@Sergius-MacBook-Pro.local>
2020-03-30 18:04:07 +0300

    Finished scoring functionalities/ tested, for the external access point

--- FirstQuiz/Game.swift
+++ FirstQuiz/Game.swift
@@ -16,8 +16,15 @@ public class Game<Question, Answer, R: Router> where R.Question == Question, R.A
     }
 }
 
-public func startGame<Question, Answer, R: Router> (questions: [Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
-    let flow = Flow(questions: questions, router: router, scoring:  {_ in 1 })
+public func startGame<Question, Answer: Equatable, R: Router> (questions: [Question], router: R, correctAnswers: [Question: Answer]) -> Game<Question, Answer, R> where R.Question == Question, R.Answer == Answer {
+    let flow = Flow(questions: questions, router: router, scoring:  {scoring($0, correctAnswers: correctAnswers)})
     flow.start()
     return Game(flow: flow)
+}
+
+
+private func scoring<Question, Answer: Equatable>(_ answers: [Question: Answer], correctAnswers: [Question: Answer]) -> Int {
+    return answers.reduce(0) { (score, tuple) in
+        return score + (correctAnswers[tuple.key] == tuple.value ? 1 : 0)
+    }
 }
